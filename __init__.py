67c1d981798ca3634a233c223b76058284c4e5c2
Ken Simpson <ksimpson@ttul.org>
2026-02-05 14:20:57 -0800

    test: tolerate missing comfy_api in package init

--- __init__.py
+++ __init__.py
@@ -5,6 +5,14 @@ _MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 if _MODULE_DIR not in sys.path:
     sys.path.append(_MODULE_DIR)
 
-from nodes_taylor_attention import TaylorAttentionExtension, comfy_entrypoint  # noqa: E402
-
-__all__ = ["TaylorAttentionExtension", "comfy_entrypoint"]
+try:
+    from nodes_taylor_attention import TaylorAttentionExtension, comfy_entrypoint  # noqa: E402
+except ModuleNotFoundError as exc:
+    if exc.name == "comfy_api":
+        TaylorAttentionExtension = None
+        comfy_entrypoint = None
+        __all__ = []
+    else:
+        raise
+else:
+    __all__ = ["TaylorAttentionExtension", "comfy_entrypoint"]
